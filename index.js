faf2c88d2b75ec1a3fb87e2d46d9665214ef9868
Kishan Sharma <21cs43@ecajmer.ac.in>
2024-09-28 07:33:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,15 @@
     document.getElementsByClassName("dates")[2].innerHTML = "hi kishan";
     // }
 // getCurrntTime();
+
+// set active class dynamically
+     document.addEventListener("DOMContentLoaded", function() {
+       const currentLocation = location.href;
+       const menuItem = document.querySelectorAll("nav ul li a");
+       const menuLength = menuItem.length;
+       for (let i = 0; i < menuLength; i++) {
+         if (menuItem[i].href === currentLocation) {
+           menuItem[i].className = "active";
+         }
+       }
+      });
